f3e9bea52c3a23137780ef59401fd6b8e06fc60b
Huda Yousif <hjdoski@gmail.com>
2019-10-22 05:48:42 +0000

    Automatically backed up by Learn

--- lib/concers/memorable.rb
+++ lib/concers/memorable.rb
@@ -1,6 +1,19 @@
-
-
 module Memorable
   
-  
-end 
+  module ClassMethods
+    
+    def reset_all
+      self.all.clear
+    end
+ 
+    def count
+      self.all.count
+    end
+  end
+ 
+  module InstanceMethods
+    def initialize
+      # some more code coming soon!
+    end
+  end
+end
