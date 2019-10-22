906d0a18969a1809087cdd169243d0e6b4ba7301
Huda Yousif <hjdoski@gmail.com>
2019-10-22 05:51:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ require_relative './concerns/paramable.rb'
 class Song
   
   extend Memorable::ClassMethods
-include Memorable::InstanceMethods
+  include Memorable::InstanceMethods
 
   attr_accessor :name
   attr_reader :artist
